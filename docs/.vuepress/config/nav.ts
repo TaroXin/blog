85b2e963db55db2cabc5ab9b18ca43b3e5c185e9
TaroXin <15029352778@163.com>
2023-03-02 00:41:50 +0800

    feat: 添加主页的Nav

--- docs/.vuepress/config/nav.ts
+++ docs/.vuepress/config/nav.ts
@@ -5,6 +5,10 @@ import { NavItem } from "vuepress/config";
 export default <Array<NavItem>>[
   { text: "泰罗GO", link: "/" },
   {
+    text: '我的收藏',
+    link: "/pages/2d6556/"
+  },
+  {
     text: "所有文章",
     link: "/archives/"
   },
