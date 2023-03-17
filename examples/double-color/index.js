aad8df09e6592f5071c6b654f6ea755c3a85c651
TaroXin <15029352778@163.com>
2023-03-17 09:21:03 +0800

    feat: 更新了一下

--- examples/double-color/index.js
+++ examples/double-color/index.js
@@ -100,16 +100,65 @@ function compressAll() {
 
 
 // 执行解压操作
-const brFile = fs.readFileSync('./hello-2.txt.br')
-const gzipFile = fs.readFileSync('./hello-2.txt.gz')
-const deflateFile = fs.readFileSync('./hello-2.txt.def')
+// const brFile = fs.readFileSync('./hello-2.txt.br')
+// const gzipFile = fs.readFileSync('./hello-2.txt.gz')
+// const deflateFile = fs.readFileSync('./hello-2.txt.def')
+//
+// const brFileStr = zlib.brotliDecompressSync(brFile).toString()
+// const gzipFileStr = zlib.gunzipSync(gzipFile).toString()
+// const deflateFileStr = zlib.inflateSync(deflateFile).toString()
+//
+// console.log(brFileStr)
+// console.log(gzipFileStr)
+// console.log(deflateFileStr)
+//
+// console.log(brFileStr === gzipFileStr, brFileStr === deflateFileStr) // true, true
 
-const brFileStr = zlib.brotliDecompressSync(brFile).toString()
-const gzipFileStr = zlib.gunzipSync(gzipFile).toString()
-const deflateFileStr = zlib.inflateSync(deflateFile).toString()
+function getFirstSend() {
+  const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFG'
+  const doubleColorBallStr = getDoubleColorBall(500000).join('')
+  let resultStr = ''
+  for (let i = 0; i < doubleColorBallStr.length; i+=2) {
+    const number = doubleColorBallStr[i] + doubleColorBallStr[i+1]
+    resultStr += letters[parseInt(number) - 1]
+  }
+  return resultStr
+}
 
-console.log(brFileStr)
-console.log(gzipFileStr)
-console.log(deflateFileStr)
+// const firstPrize = getFirstSend()
+// fs.writeFileSync('./first-send.txt.br', zlib.brotliCompressSync(firstPrize))
 
-console.log(brFileStr === gzipFileStr, brFileStr === deflateFileStr) // true, true
+function letterToCode(letterStr) {
+  const result = []
+  const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFG'
+  for (let i = 0; i < letterStr.length; i++) {
+    result.push((letters.indexOf(letterStr[i]) + 1).toString().padStart(2, '0'))
+  }
+  return result
+}
+
+function decodeData(brFile) {
+  const result = []
+  const content = zlib.brotliDecompressSync(brFile).toString()
+  // 按照七位每注的结构拆分
+  for (let i = 0; i < content.length; i += 7) {
+    result.push(content.slice(i, i + 8))
+  }
+  return result
+}
+
+const firstSend = fs.readFileSync('./first-send.txt.br')
+const firstDataList = decodeData(firstSend)
+console.log(firstDataList.length) // 500000
+
+function random(count) {
+  let result = []
+  for (let i = 0; i < count; i++) {
+    const index = Math.floor(Math.random() * firstDataList.length)
+    console.log(firstDataList[index])
+    result.push(letterToCode(firstDataList[index]))
+  }
+  return result
+}
+
+console.log(random(2))
